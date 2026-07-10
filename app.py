f49ddd6e5e4f08f71a493bd5535a7825f560c00f
SomethingCFW <luisvaldes2501@gmail.com>
2026-07-10 13:02:12 -0400

    Update app.py

--- app.py
+++ app.py
@@ -183,4 +183,4 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()   # create tables if they don't exist
 
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=False)
